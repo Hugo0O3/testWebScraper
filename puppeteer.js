a9cccabb2b718d28cd776256f809ec93c7ef2a6f
Hugo0O3 <hugomancini2.0@gmail.com>
2025-03-08 21:15:22 +0100

    test

--- puppeteer.js
+++ puppeteer.js
@@ -1,15 +1,19 @@
-import * as puppeteer from "puppeteer"
+import * as puppeteer from "puppeteer";
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true })
-
+    const browser = await puppeteer.launch({ headless: true }) // Mettre false c'est une dinguerie
     const page = await browser.newPage()
 
-    await page.goto("https://deux.io/web-scraping-puppeteer/", { waitUntil: 'domcontentloaded' })
+    await page.goto('https://www.floorballbelgium.be/en/competitions', { waitUntil: 'networkidle2' })
 
-    const title = await page.title()
+    await page.waitForSelector('.title_comp')
 
-    console.log("Titre de la page :", title)
+    const leagues = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.title_comp'))
+            .map(league => league.innerText.trim())
+    })
 
-    await browser.close()
-})()
+    console.log("Ligues disponibles pour 2024-2025 :", leagues)
+
+    await browser.close();
+})();
